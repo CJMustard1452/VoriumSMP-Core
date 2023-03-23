55491e7270ac777407078f7f5be5d84daeab47cb
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-23 15:32:04 -0500

    Add files via upload

---
+++ VoriumSMP-Core.ts
@@ -0,0 +1,43 @@
+import { events } from "bdsx/event";
+import { command } from "bdsx/command";
+import { CommandContext } from "bdsx/bds/command";
+
+events.serverOpen.on(() => {
+    console.log("[VoriumCore] Enabled.");
+    command.register('alliance', 'Provides access alliance commands.');
+});
+
+events.command.on((command, player, commandContext)=>{
+    if(command.split(' ')[0].slice(1) == 'alliance') allianceCommand(command, player, commandContext)
+    return 0;
+});
+
+
+function allianceCommand(command: string, player: string, commandContext: CommandContext){
+    switch(command.split(' ')[1]){
+        default:
+            console.log('NOTHING')
+            break;
+        case 'help': case '?':
+            //Alliance Help
+            break;
+        case 'create':
+            //Create Alliance
+            break;
+        case 'delete':
+            //Delete Alliance
+            break;
+        case 'leave':
+            //Leave Alliance
+            break;
+        case 'invite':
+            //Invite Player
+            break;
+        case 'kick':
+            //Remove Player
+            break;
+
+    }
+}
+
+
