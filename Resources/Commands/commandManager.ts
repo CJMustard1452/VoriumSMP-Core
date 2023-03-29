fdf0105164f731dcce39679b8b62a4d731da2d76
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-28 23:29:29 -0500

    Delete Resources directory

--- Resources/Commands/commandManager.ts
+++
@@ -1,9 +0,0 @@
-import { CommandContext } from "bdsx/bds/command";
-module.exports = {
-    execute(command: string, player: string, commandContext: CommandContext) {
-        switch(command.split(' ')[0].slice(1)){
-            case 'alliance':
-                require('./allianceCommand').execute(command, player, commandContext)
-        }
-    }
-}
--- Resources/Events/serverOpen.ts
+++
@@ -1,7 +0,0 @@
-import { command } from "bdsx/command";
-module.exports = {
-    execute(){
-        console.log("[VoriumCore] Enabled.");
-        command.register('alliance', 'Provides access alliance commands.');
-    }
-}
