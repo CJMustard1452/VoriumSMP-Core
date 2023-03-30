7994c3470671401a394e578b352ac82a694af399
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-29 23:07:31 -0500

    imp command

---
+++ src/CommandManagers/AllianceSubcommands/remove.ts
@@ -0,0 +1,9 @@
+import { CommandContext } from "bdsx/bds/command";
+import { Player } from "bdsx/bds/player";
+
+export class allianceRemove{
+
+    public static init(player: Player, commandContext: CommandContext, allianceData: any): void {
+
+    }
+}
