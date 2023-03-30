ef9bdf872a81d1d5add4feb12e2337f5c39fad04
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-29 23:08:15 -0500

    imp info

--- src/PluginManagers/openServer.ts
+++ src/PluginManagers/openServer.ts
@@ -1,7 +1,7 @@
 import { mkdirSync, openSync, writeFileSync, existsSync } from 'fs'
 import { command } from "bdsx/command";
 export class openServer {
-    
+
     public static init(): void {
         console.log('VoriumSMP-Core has been enabled.');
         if (!existsSync('../plugin_data/VoriumSMP-Core/alliancedata.json')) openServer.registerPluginData();
@@ -20,6 +20,7 @@ export class openServer {
             .overload(param => {}, { option: command.enum("create", "create"), text: command.enum("alliance name")})
             .overload(param => {}, { option: command.enum("invite", "invite"), text: command.enum("playername")})
             .overload(param => {}, { option: command.enum("remove", "remove"), text: command.enum("playername") })
+            .overload(param => {}, { option: command.enum("info", "info") })
             .overload(param => {}, { option: command.enum("leave", "leave") })
             .overload(param => {}, { option: command.enum("disband", "disband") });
     }
