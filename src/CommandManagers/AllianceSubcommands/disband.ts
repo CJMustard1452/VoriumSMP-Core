5151bc90b2e71760695c121945bd202abf704aa6
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-29 23:06:23 -0500

    imp player

--- src/CommandManagers/AllianceSubcommands/disband.ts
+++ src/CommandManagers/AllianceSubcommands/disband.ts
@@ -1,12 +1,11 @@
 import { CommandContext } from "bdsx/bds/command";
+import { Player } from "bdsx/bds/player";
 import { bedrockServer } from "bdsx/launcher";
 import { writeFileSync } from "fs";
 
 export class allianceDisband{
 
-    public static init(commandContext: CommandContext, allianceData: any): void {
-        const player = commandContext.origin.getEntity();
-        if(!player?.isPlayer()) return;
+    public static init(player: Player,commandContext: CommandContext, allianceData: any): void {
         if(!allianceData['players'][player.getName()]['alliance']) return player.sendMessage('§8(§3Vorium-SMP§8) §cYou are not in an alliance.');
         if(allianceData['alliances'][allianceData['players'][player.getName()]['alliance']]['founder'] !== player.getName()) return player.sendMessage('§8(§3Vorium-SMP§8) §cYou do not own this alliance.');
         bedrockServer.serverInstance.getPlayers().forEach(p => p.sendMessage(`§8(§3Vorium-SMP§8) §c${player.getName()} §ahas just disbanded §c${allianceData['players'][player.getName()]['alliance']}§a.`));
