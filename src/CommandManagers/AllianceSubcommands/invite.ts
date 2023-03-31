db1da513b20cf76f51736a4498c80ed3a1d353de
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-03-31 16:15:04 -0500

    Delete src directory

--- src/CommandManagers/AllianceSubcommands/invite.ts
+++
@@ -1,9 +0,0 @@
-import { CommandContext } from "bdsx/bds/command";
-import { Player } from "bdsx/bds/player";
-
-export class allianceInvite{
-
-    public static init(player: Player, commandContext: CommandContext, allianceData: any): void {
-        
-    }
-}
--- src/CommandManagers/AllianceSubcommands/remove.ts
+++
@@ -1,9 +0,0 @@
-import { CommandContext } from "bdsx/bds/command";
-import { Player } from "bdsx/bds/player";
-
-export class allianceRemove{
-
-    public static init(player: Player, commandContext: CommandContext, allianceData: any): void {
-
-    }
-}
