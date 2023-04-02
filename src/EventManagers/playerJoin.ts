61a9ed077e0e1e36224d439f20dc277ee3859d19
CJMustard1452 <80650734+CJMustard1452@users.noreply.github.com>
2023-04-02 01:44:34 -0500

    Prettier

---
+++ src/EventManagers/playerJoin.ts
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from 'fs'
+import { Player } from 'bdsx/bds/player';
+
+export class playerJoin {
+
+    allianceData = JSON.parse(readFileSync('../plugin_data/VoriumSMP-Core/alliancedata.json', 'utf-8'));
+
+    public constructor(player: Player) {
+        if(!this.allianceData['players'][`${player.getName().toLowerCase()}`]) this.registerPlayer(player);
+    }
+
+    private registerPlayer(player: Player): void {
+        player.sendMessage('§8(§3Vorium-SMP§8) §aWelcome, we are currently setting up player data associated with this account, to get started run §c/alliance§a.');
+        this.allianceData['players'][`${player.getName().toLowerCase()}`] = {};
+        this.allianceData['players'][`${player.getName().toLowerCase()}`]["alliance"] = false;
+        this.allianceData['players'][`${player.getName().toLowerCase()}`]["invites"] = [];
+        writeFileSync('../plugin_data/VoriumSMP-Core/alliancedata.json', JSON.stringify(this.allianceData), 'utf-8');
+    }
+}
---
+++ src/PluginManagers/commandManager.ts
@@ -0,0 +1,11 @@
+import { allianceCommand } from "../CommandManagers/allianceCommand";
+import { CommandContext } from "bdsx/bds/command";
+
+export class commandManager {
+
+    public static init(command: String, commandContext: CommandContext): void {
+        if(command.split(' ')[0].slice(1) == 'alliance') {
+            new allianceCommand(command, commandContext);
+        }
+    }
+}
