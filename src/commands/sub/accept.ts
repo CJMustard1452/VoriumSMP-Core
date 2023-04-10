8fd21ad822c498c280e8545f28eac3f87b39d7ae
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2023-04-10 15:10:31 -0400

    fix this jawn

--- src/commands/sub/accept.ts
+++ src/commands/sub/accept.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CommandContext } from "bdsx/bds/command";
 import { bedrockServer } from "bdsx/launcher";
 import { Player } from "bdsx/bds/player";
@@ -12,10 +11,10 @@ import { cache } from "../alliance";
 
 
 const accept = (user: User, params: Record<string, any>) => {
-    if (AllianceModule.ownsAlliance(user.name)) {
-        user.message(Messages.ownAlliance)
-        return
-    }
+    // if (AllianceModule.ownsAlliance(user.name)) {
+    //     user.message(Messages.ownAlliance)
+    //     return
+    // }
     if(!params.name) {
         user.message(Messages.noName);
         return
@@ -35,42 +34,4 @@ const accept = (user: User, params: Record<string, any>) => {
     broadcast(Messages.newMember(user.invite, user.name))
 }
 
-=======
-import { CommandContext } from "bdsx/bds/command";
-import { bedrockServer } from "bdsx/launcher";
-import { Player } from "bdsx/bds/player";
-import { writeFileSync } from "fs";
-import { create } from "ts-node";
-import AllianceModule from "../../lib/AllianceModule";
-import User from "../../lib/User";
-import { broadcast } from "../../lib/Util";
-import { Messages } from "../../lib/Messages";
-import { cache } from "../alliance";
-
-
-const accept = (user: User, params: Record<string, any>) => {
-    if (AllianceModule.ownsAlliance(user.name)) {
-        user.message(Messages.ownAlliance)
-        return
-    }
-    if(!params.name) {
-        user.message(Messages.noName);
-        return
-    }
-    if(!cache[params.name]) {
-        user.message(Messages.noInviteAlliance)
-        return;
-    }
-
-    if(!cache[params.name].includes(user.name)) {
-        user.message(Messages.noInviteAlliance);
-        return;
-    }
-
-    cache[params.name] = cache[params.name].filter(d => d !== user.name);
-    AllianceModule.addMember(params.name, user.name);
-    broadcast(Messages.newMember(params.name, user.name))
-}
-
->>>>>>> 32f5d1ed9645b60e565ffe1bc765fc0818b23598
 export default accept;
